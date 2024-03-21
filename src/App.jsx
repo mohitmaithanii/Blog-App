8603bfa9272b56ff9d2cbdc927b17c75e2667eeb
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-03-21 18:01:34 +0530

    Update App.jsx to include authentication and layout; update index.css to use Tailwind CSS
    The changes include:
    - Importing necessary dependencies and hooks in App.jsx
    - Adding useEffect hook to handle authentication
    - Adding Header, Footer, and Outlet components from react-router-dom
    - Adding a loading state to handle the delay in authentication
    - Updating index.css to use Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-	console.log(import.meta.env.VITE_APPWRITE_URL);
-	return (
-		<>
-			<h1>Blog App with appwrite</h1>
-		</>
-	);
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		authService
+			.getCurrentUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoading(false));
+	}, []);
+
+	return !loading ? (
+		<div className="min-h-screen flex flex-wrap content-between bg-gray-300">
+			<div className="w-full block">
+				<Header />
+				<main>
+					<Outlet />
+				</main>
+				<Footer />
+			</div>
+		</div>
+	) : null;
 }
 
 export default App;
