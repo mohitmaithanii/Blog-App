1b3e72d12893ccbde896e1aa0b966b72d659a63a
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-03-22 16:12:57 +0530

    "Update Header and Footer components with navigation and layout improvements"
    This commit updates the `Header` and `Footer` components by adding navigation links, a logo, and a logout button. The `Header` component now uses the `Container`, `Logo`, and `LogoutBtn` components, and the `Footer` component includes a layout with four columns for company, support, and legal information. The navigation links are conditionally rendered based on the authentication status.

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
+
+function LogoutBtn() {
+	const dispatch = useDispatch();
+	const logoutHandler = () => {
+		authService.logout().then(() => {
+			dispatch(logout());
+		});
+	};
+	return (
+		<button
+			className="px-6 py-2 duration-200 rounded-full inline-bock hover:bg-blue-100"
+			onClick={logoutHandler}
+		>
+			Logout
+		</button>
+	);
+}
+
+export default LogoutBtn;
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,5 @@
+function Container({ children }) {
+	return <div className="w-full px-4 mx-auto max-w-7">{children}</div>;
+}
+
+export default Container;
